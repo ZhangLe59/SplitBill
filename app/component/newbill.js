d0bf6d6c49c9feb29bf80d961fd6e10b87af6862
le.zhang <le.zhang@silot.ai>
2019-01-04 14:49:20 +0800

    update create bill ui
    update create bill ui

--- app/component/newbill.js
+++ app/component/newbill.js
@@ -26,22 +26,24 @@ const styles = StyleSheet.create({
 class NewBillScreen extends React.Component {
     constructor(props){
         super(props);
+        this.state = { billtext: '', username: '' }
     }
 
     onPress = () => {
-        console.log("");
+        this.setState({ billtext: this.refs.billname.state.text, username: this.refs.yourname.state.text });
     }
       
     render() {
       return (
         <View style={styles.container}>
-          <LabelTextRowScreen name='Bill Name' />
-          <LabelTextRowScreen name='Your Name' />
+          <LabelTextRowScreen name='Bill Name' ref="billname" />
+          <LabelTextRowScreen name='Your Name' ref="yourname" />
           <TouchableOpacity style={styles.button}
                 onPress={this.onPress} >
                 <Text>Create A New Bill</Text>
           </TouchableOpacity>
-          <Text ></Text>
+          <Text >{ this.state.billtext }</Text>
+          <Text >{ this.state.username }</Text>
         </View>
       );
     }
